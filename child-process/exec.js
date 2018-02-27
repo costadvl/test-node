9194de98fbf858c369715bf97fbf4e638d382247
costadvl <costadvl@gmail.com>
2018-02-28 00:29:14 +0100

    feat(None): None
    None
    
    None

---
+++ child-process/exec.js
@@ -0,0 +1,21 @@
+const { exec, spawn } = require('child_process');
+
+// it a good choice if the return is not big, since it buffers the output
+exec('find . -type f | wc -l', (err, stdout, stderr) => {
+    if(err) {
+        console.error(`exec error: ${err}`);
+        return;
+    }
+
+    console.log(`Number of files ${stdout}`);
+});
+
+const child = spawn('find', ['.', '-type', 'f'], {
+    stdio: 'inherit'
+});
+
+const child2 = spawn('find . -type f | wc -l', {
+    stdio: 'inherit',
+    shell: true,
+    cwd: '/c/Users'
+});
