64ea83654566cf793e3f7f00ad3efa5d0992daae
costadvl <costadvl@gmail.com>
2018-03-01 09:54:46 +0100

    feat(Cluster): Cluster features
    Added server and cluster process examples
    
    BREAKING CHANGE: None

--- child-process/exec.js
+++ child-process/exec.js
@@ -1,21 +1,27 @@
 const { exec, spawn } = require('child_process');
 
 // it a good choice if the return is not big, since it buffers the output
-exec('find . -type f | wc -l', (err, stdout, stderr) => {
-    if(err) {
-        console.error(`exec error: ${err}`);
-        return;
-    }
+// exec('find . -type f | wc -l', (err, stdout, stderr) => {
+//     if(err) {
+//         console.error(`exec error: ${err}`);
+//         return;
+//     }
 
-    console.log(`Number of files ${stdout}`);
-});
+//     console.log(`Number of files ${stdout}`);
+// });
 
-const child = spawn('find', ['.', '-type', 'f'], {
-    stdio: 'inherit'
-});
+// const child = spawn('find', ['.', '-type', 'f'], {
+//     stdio: 'inherit'
+// });
 
-const child2 = spawn('find . -type f | wc -l', {
+// const child2 = spawn('find . -type f | wc -l', {
+//     stdio: 'inherit',
+//     shell: true,
+//     cwd: '/c/Users'
+// });
+
+const child3 = spawn('echo %PATH%', {
     stdio: 'inherit',
     shell: true,
-    cwd: '/c/Users'
+    env: {}
 });
